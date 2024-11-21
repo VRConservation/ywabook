9616e8d4ddefdf578c5f6f88423b89663cdfb4cb
VRConservation <vance.russell@outlook.com>
2024-11-21 11:00:04 +0000

    Chapter edits

---
+++ update.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+# Save the original directory
+orig_dir = os.getcwd()
+
+# run the ghp-import -n -p -f _build/html
+subprocess.run(["ghp-import", "-n", "-p", "-f", "_build/html"], check=True)
+
+# Run the command jb build --all . after the loop
+subprocess.run(["jb", "build", "--all", "."], check=True)
+
+# Push and sync the repository to GitHub make sure to change the 'Update repo' message
+subprocess.run(["git", "add", "."], check=True)
+subprocess.run(["git", "commit", "-m", "Chapter edits"], check=True)
+subprocess.run(["git", "push"], check=True)
+
+# Open local build in web browser
+# Define the path to the intro.html file
+file_path = r'D:\OneDrive\1_Consulting\YWA\Report\ywabook\_build\html\index.html'
+
+# Define the command to open the file in Brave or Chrome
+# For Brave
+command_brave = f'start brave "{file_path}"'
+
+# For Chrome
+# command_chrome = f'start chrome "{file_path}"'
+
+# Execute the command in Brave. Use command_chrome for Chrome
+subprocess.run(command_brave, shell=True)  # Use command_chrome for Chrome
